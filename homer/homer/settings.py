a2fda725d7bf017a42aceb1c0f1f54d2237e239f
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-05-21 19:00:03 +0200

    hardcode engine name in settings.py, delete it from .env

--- homer/homer/settings.py
+++ homer/homer/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'homer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("DB_ENGINE"),
+        'ENGINE': "django.db.backends.postgresql",
         'NAME':  os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_USER_PASSWORD"),
